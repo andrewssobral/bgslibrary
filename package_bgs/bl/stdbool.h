602d7cef90a365ad68cd53b8dc651d7dcd09c367
Andrews Sobral <andrewssobral@gmail.com>
2014-04-23 17:46:47 +0200

    Fix

--- package_bgs/bl/stdbool.h
+++ package_bgs/bl/stdbool.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#ifndef false
-  #define false   0
-#endif
+//#ifndef false
+//  #define false   0
+//#endif
 
-#ifndef true
-  #define true    1
-#endif
+//#ifndef true
+//  #define true    1
+//#endif
 
-#ifndef bool
-  #define bool int
-#endif
+//#ifndef bool
+//  #define bool int
+//#endif
 
 // #ifdef _WIN32
 // #endif
