edeaca8ab0b7f59bfc78e665f6f27d78797578b4
Andrews Sobral <andrewssobral@gmail.com>
2015-05-02 01:18:13 +0200

    Fix

--- vs2010mfc/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// bgslibrary_vs2010_mfc.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
