b2eb4db4ff650b01b9a520e4deca51e18744c930
Andrews Sobral <andrewssobral@gmail.com>
2017-07-13 15:31:00 +0200

    Fix for Python wrapper.

--- Demo.py
+++ Demo.py
@@ -55,13 +55,17 @@ while not capture.isOpened():
 	cv2.waitKey(1000)
 	print "Wait for the header"
 
-pos_frame = capture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+#pos_frame = capture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+#pos_frame = capture.get(cv2.CV_CAP_PROP_POS_FRAMES)
+pos_frame = capture.get(1)
 while True:
 	flag, frame = capture.read()
 	
 	if flag:
 		cv2.imshow('video', frame)
-		pos_frame = capture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+		#pos_frame = capture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+		#pos_frame = capture.get(cv2.CV_CAP_PROP_POS_FRAMES)
+		pos_frame = capture.get(1)
 		#print str(pos_frame)+" frames"
 		
 		img_output = bgs.apply(frame)
@@ -71,15 +75,19 @@ while True:
 		cv2.imshow('img_bgmodel', img_bgmodel)
 
 	else:
-		capture.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
-		print "Frame is not ready"
+		#capture.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+		#capture.set(cv2.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+		#capture.set(1, pos_frame-1)
+		#print "Frame is not ready"
 		cv2.waitKey(1000)
-		# break
+		break
 	
 	if 0xFF & cv2.waitKey(10) == 27:
 		break
 	
-	if capture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == capture.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
-		break
+	#if capture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == capture.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+	#if capture.get(cv2.CV_CAP_PROP_POS_FRAMES) == capture.get(cv2.CV_CAP_PROP_FRAME_COUNT):
+	#if capture.get(1) == capture.get(cv2.CV_CAP_PROP_FRAME_COUNT):
+		#break
 
 cv2.destroyAllWindows()
