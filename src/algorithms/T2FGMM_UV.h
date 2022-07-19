05f54e6d2977de474d8209d91fc463a8aa4c866a
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2022-07-20 01:26:59 +0200

    Updated opencv compatibility of the bgs algorithms

--- src/algorithms/T2FGMM_UV.h
+++ src/algorithms/T2FGMM_UV.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "opencv2/core/version.hpp"
-#if CV_MAJOR_VERSION >= 2 && CV_MAJOR_VERSION <= 3
+#if CV_MAJOR_VERSION == 2 || CV_MAJOR_VERSION == 3
 
 #include "IBGS.h"
 #include "T2F/T2FGMM.h"
--- src/algorithms/algorithms.h
+++ src/algorithms/algorithms.h
@@ -9,33 +9,33 @@
 #include "AdaptiveBackgroundLearning.h"
 #include "AdaptiveSelectiveBackgroundLearning.h"
 #include "KNN.h" // Only for OpenCV >= 3
-#include "GMG.h" // Only for OpenCV >= 2.4.3
-#include "DPAdaptiveMedian.h"
-#include "DPGrimsonGMM.h"
-#include "DPZivkovicAGMM.h"
-#include "DPMean.h"
-#include "DPWrenGA.h"
-#include "DPPratiMediod.h"
-#include "DPEigenbackground.h"
-#include "DPTexture.h"
-#include "T2FGMM_UM.h"
-#include "T2FGMM_UV.h"
-#include "T2FMRF_UM.h"
-#include "T2FMRF_UV.h"
-#include "FuzzySugenoIntegral.h"
-#include "FuzzyChoquetIntegral.h"
-#include "LBSimpleGaussian.h"
-#include "LBFuzzyGaussian.h"
-#include "LBMixtureOfGaussians.h"
-#include "LBAdaptiveSOM.h"
-#include "LBFuzzyAdaptiveSOM.h"
-#include "LBP_MRF.h"
-#include "MultiLayer.h"
+#include "GMG.h" // Only for OpenCV >= 2.4.3 and OpenCV < 3
+#include "DPAdaptiveMedian.h" // Only for OpenCV 2 or 3
+#include "DPGrimsonGMM.h" // Only for OpenCV 2 or 3
+#include "DPZivkovicAGMM.h" // Only for OpenCV 2 or 3
+#include "DPMean.h" // Only for OpenCV 2 or 3
+#include "DPWrenGA.h" // Only for OpenCV 2 or 3
+#include "DPPratiMediod.h" // Only for OpenCV 2 or 3
+#include "DPEigenbackground.h" // Only for OpenCV 2 or 3
+#include "DPTexture.h" // Only for OpenCV 2 or 3
+#include "T2FGMM_UM.h" // Only for OpenCV 2 or 3
+#include "T2FGMM_UV.h" // Only for OpenCV 2 or 3
+#include "T2FMRF_UM.h" // Only for OpenCV 2 or 3
+#include "T2FMRF_UV.h" // Only for OpenCV 2 or 3
+#include "FuzzySugenoIntegral.h" // Only for OpenCV 2 or 3
+#include "FuzzyChoquetIntegral.h" // Only for OpenCV 2 or 3
+#include "LBSimpleGaussian.h" // Only for OpenCV 2 or 3
+#include "LBFuzzyGaussian.h" // Only for OpenCV 2 or 3
+#include "LBMixtureOfGaussians.h" // Only for OpenCV 2 or 3
+#include "LBAdaptiveSOM.h" // Only for OpenCV 2 or 3
+#include "LBFuzzyAdaptiveSOM.h" // Only for OpenCV 2 or 3
+#include "LBP_MRF.h" // Only for OpenCV 2 or OpenCV <= 3.4.7
+#include "MultiLayer.h" // Only for OpenCV 2 or OpenCV <= 3.4.7
 #include "PixelBasedAdaptiveSegmenter.h"
-#include "VuMeter.h"
-#include "KDE.h"
-#include "IndependentMultimodal.h"
-#include "MultiCue.h"
+#include "VuMeter.h" // Only for OpenCV 2 or 3
+#include "KDE.h" // Only for OpenCV 2 or 3
+#include "IndependentMultimodal.h" // Only for OpenCV 2 or 3
+#include "MultiCue.h" // Only for OpenCV 2 or 3
 #include "SigmaDelta.h"
 #include "SuBSENSE.h"
 #include "LOBSTER.h"
