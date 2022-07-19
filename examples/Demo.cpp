081d4d4a67b5fcadd3379fad6e72f93066c16524
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2022-07-20 00:50:10 +0200

    Applied code format to demo examples

--- examples/Demo.cpp
+++ examples/Demo.cpp
@@ -11,82 +11,82 @@
 #define CV_CAP_PROP_FRAME_COUNT cv::CAP_PROP_FRAME_COUNT
 #endif
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
+	std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
 
-  cv::VideoCapture capture;
+	cv::VideoCapture capture;
 
-  if (argc > 1)
-  {
-    std::cout << "Openning: " << argv[1] << std::endl;
-    capture.open(argv[1]);
-  }
-  else
-    capture.open(0);
+	if (argc > 1)
+	{
+		std::cout << "Openning: " << argv[1] << std::endl;
+		capture.open(argv[1]);
+	}
+	else
+		capture.open(0);
 
-  if (!capture.isOpened())
-  {
-    std::cerr << "Cannot initialize video!" << std::endl;
-    return -1;
-  }
+	if (!capture.isOpened())
+	{
+		std::cerr << "Cannot initialize video!" << std::endl;
+		return -1;
+	}
 
-  /* Background Subtraction Methods */
-  auto algorithmsName = BGS_Factory::Instance()->GetRegisteredAlgorithmsName();
-  
-  std::cout << "List of available algorithms:" << std::endl;
-  std::copy(algorithmsName.begin(), algorithmsName.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-  
-  for (const std::string& algorithmName : algorithmsName)
-  {
-    std::cout << "Running " << algorithmName << std::endl;
-    auto bgs = BGS_Factory::Instance()->Create(algorithmName);
+	/* Background Subtraction Methods */
+	auto algorithmsName = BGS_Factory::Instance()->GetRegisteredAlgorithmsName();
 
-    cv::Mat img_input;
-    
-    capture.set(CV_CAP_PROP_POS_FRAMES, 0); // Set index to 0 (start frame)
-    auto frame_counter = 0;
-    std::cout << "Press 's' to stop:" << std::endl;
-    auto key = 0;
-    while (key != 's')
-    {
-      // Capture frame-by-frame
-      capture >> img_input;
-      frame_counter += 1;
+	std::cout << "List of available algorithms:" << std::endl;
+	std::copy(algorithmsName.begin(), algorithmsName.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
 
-      if (img_input.empty()) break;
+	for (const std::string& algorithmName : algorithmsName)
+	{
+		std::cout << "Running " << algorithmName << std::endl;
+		auto bgs = BGS_Factory::Instance()->Create(algorithmName);
 
-      cv::resize(img_input, img_input, cv::Size(380, 240), 0, 0, CV_INTER_LINEAR);
-      cv::imshow("input", img_input);
+		cv::Mat img_input;
 
-      cv::Mat img_mask;
-      cv::Mat img_bkgmodel;
-      try
-      {
-        bgs->process(img_input, img_mask, img_bkgmodel); // by default, it shows automatically the foreground mask image
+		capture.set(CV_CAP_PROP_POS_FRAMES, 0); // Set index to 0 (start frame)
+		auto frame_counter = 0;
+		std::cout << "Press 's' to stop:" << std::endl;
+		auto key = 0;
+		while (key != 's')
+		{
+			// Capture frame-by-frame
+			capture >> img_input;
+			frame_counter += 1;
 
-        //if(!img_mask.empty())
-        //  cv::imshow("Foreground", img_mask);
-        //  do something
-      }
-      catch (std::exception& e)
-      {
-        std::cout << "Exception occurred" << std::endl;
-        std::cout << e.what() << std::endl;
-      }
+			if (img_input.empty()) break;
 
-      key = cv::waitKey(33);
-    }
+			cv::resize(img_input, img_input, cv::Size(380, 240), 0, 0, CV_INTER_LINEAR);
+			cv::imshow("input", img_input);
 
-    std::cout << "Press 'q' to exit, or anything else to move to the next algorithm:" << std::endl;
-    key = cv::waitKey(0);
-    if (key == 'q')
-      break;
+			cv::Mat img_mask;
+			cv::Mat img_bkgmodel;
+			try
+			{
+				bgs->process(img_input, img_mask, img_bkgmodel); // by default, it shows automatically the foreground mask image
 
-    cv::destroyAllWindows();
-  }
+				//if(!img_mask.empty())
+				//  cv::imshow("Foreground", img_mask);
+				//  do something
+			}
+			catch (std::exception& e)
+			{
+				std::cout << "Exception occurred" << std::endl;
+				std::cout << e.what() << std::endl;
+			}
 
-  capture.release();
+			key = cv::waitKey(33);
+		}
 
-  return 0;
+		std::cout << "Press 'q' to exit, or anything else to move to the next algorithm:" << std::endl;
+		key = cv::waitKey(0);
+		if (key == 'q')
+			break;
+
+		cv::destroyAllWindows();
+	}
+
+	capture.release();
+
+	return 0;
 }
--- examples/Demo2.cpp
+++ examples/Demo2.cpp
@@ -10,72 +10,72 @@
 #define CV_LOAD_IMAGE_COLOR cv::IMREAD_COLOR
 #endif
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
+	std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
 
-  std::string baseDir = "./dataset/frames";
-  if (argc > 1)
-    baseDir = argv[1];
-  std::cout << "Openning: " << baseDir << std::endl;
+	std::string baseDir = "./dataset/frames";
+	if (argc > 1)
+		baseDir = argv[1];
+	std::cout << "Openning: " << baseDir << std::endl;
 
-  /* Background Subtraction Methods */
-  auto algorithmsName = BGS_Factory::Instance()->GetRegisteredAlgorithmsName();
-  
-  std::cout << "List of available algorithms (" << algorithmsName.size() << "):" << std::endl;
-  std::copy(algorithmsName.begin(), algorithmsName.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-  
-  for (const std::string& algorithmName : algorithmsName)
-  {
-    std::cout << "Running " << algorithmName << std::endl;
-    auto bgs = BGS_Factory::Instance()->Create(algorithmName);
+	/* Background Subtraction Methods */
+	auto algorithmsName = BGS_Factory::Instance()->GetRegisteredAlgorithmsName();
 
-    auto frame_counter = 0;
-    std::cout << "Press 's' to stop:" << std::endl;
-    auto key = 0;
-    while (key != 's')
-    {
-      // Capture frame-by-frame
-      frame_counter++;
-      std::stringstream ss;
-      ss << frame_counter;
-      auto fileName = baseDir + "/" + ss.str() + ".png";
-      // std::cout << "reading " << fileName << std::endl;
+	std::cout << "List of available algorithms (" << algorithmsName.size() << "):" << std::endl;
+	std::copy(algorithmsName.begin(), algorithmsName.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
 
-      auto img_input = cv::imread(fileName, CV_LOAD_IMAGE_COLOR);
+	for (const std::string& algorithmName : algorithmsName)
+	{
+		std::cout << "Running " << algorithmName << std::endl;
+		auto bgs = BGS_Factory::Instance()->Create(algorithmName);
 
-      if (img_input.empty())
-        break;
+		auto frame_counter = 0;
+		std::cout << "Press 's' to stop:" << std::endl;
+		auto key = 0;
+		while (key != 's')
+		{
+			// Capture frame-by-frame
+			frame_counter++;
+			std::stringstream ss;
+			ss << frame_counter;
+			auto fileName = baseDir + "/" + ss.str() + ".png";
+			// std::cout << "reading " << fileName << std::endl;
 
-      cv::imshow("input", img_input);
+			auto img_input = cv::imread(fileName, CV_LOAD_IMAGE_COLOR);
 
-      cv::Mat img_mask;
-      cv::Mat img_bkgmodel;
-      try
-      {
-        bgs->process(img_input, img_mask, img_bkgmodel); // by default, it shows automatically the foreground mask image
+			if (img_input.empty())
+				break;
 
-        //if(!img_mask.empty())
-        //  cv::imshow("Foreground", img_mask);
-        //  do something
-      }
-      catch (std::exception& e)
-      {
-        std::cout << "Exception occurred" << std::endl;
-        std::cout << e.what() << std::endl;
-        break;
-      }
+			cv::imshow("input", img_input);
 
-      key = cv::waitKey(33);
-    }
+			cv::Mat img_mask;
+			cv::Mat img_bkgmodel;
+			try
+			{
+				bgs->process(img_input, img_mask, img_bkgmodel); // by default, it shows automatically the foreground mask image
 
-    std::cout << "Press 'q' to exit, or anything else to move to the next algorithm:" << std::endl;
-    key = cv::waitKey(0);
-    if (key == 'q')
-      break;
+				//if(!img_mask.empty())
+				//  cv::imshow("Foreground", img_mask);
+				//  do something
+			}
+			catch (std::exception& e)
+			{
+				std::cout << "Exception occurred" << std::endl;
+				std::cout << e.what() << std::endl;
+				break;
+			}
 
-    cv::destroyAllWindows();
-  }
+			key = cv::waitKey(33);
+		}
 
-  return 0;
+		std::cout << "Press 'q' to exit, or anything else to move to the next algorithm:" << std::endl;
+		key = cv::waitKey(0);
+		if (key == 'q')
+			break;
+
+		cv::destroyAllWindows();
+	}
+
+	return 0;
 }
