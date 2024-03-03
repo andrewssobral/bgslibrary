56d43e5c94c059ea99c67fc7beac8ebf85e0084c
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2024-03-03 21:42:40 +0100

    Updated pybgs version

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pybgs',
-    version='3.3.0.post0',
+    version='3.3.0.post1',
     author='Andrews Sobral',
     author_email='andrewssobral@gmail.com',
     url='https://github.com/andrewssobral/bgslibrary',
